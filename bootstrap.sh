65632cfcae821f0df87b65e2f7e6e11178fe6e60
taufiqpsumarna <taufiqpsumarna@gmail.com>
2026-08-09 14:23:23 +0700

    chore: fix installation scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -199,11 +199,8 @@ if installed glab; then
   warn "glab already installed"
 else
   info "Installing glab..."
-  wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg > /dev/null
-  echo "deb [arch=all,$(dpkg --print-architecture) signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
-  sudo apt update
-  sudo apt install glab
-  success "glab installed"
+  sudo snap install glab
+  sudo snap connect glab:ssh-keys
 fi
 
 # ------------------------------------------------------------------------------
